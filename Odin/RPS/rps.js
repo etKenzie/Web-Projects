a3940d23a37ff205e05fa581cdeb234206f58a2b
kenzie.evan <kenzie.evan@sunterra.id>
2024-04-09 14:29:55 -0400

    rps

---
+++ Odin/RPS/rps.js
@@ -0,0 +1,54 @@
+let choices = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    const index = Math.floor(Math.random() * choices.length);
+    return choices[index];
+};
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return "tie";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "lose";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "lose";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "lose";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "win";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "win";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "win";
+    } 
+  }
+
+function playGame() {
+    let playerScore = 0, computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Rock, Paper, or Scissors?");
+        const round = playRound(playerSelection,getComputerChoice());
+
+        console.log(round);
+        if (round == "win") {
+            playerScore += 1;
+        } else if (round == "lose") {
+            computerScore += 1;
+        } 
+    }
+
+    if (playerScore > computerScore) {
+        return "You Won the Game";
+    } else if (computerScore > playerScore) {
+        return "You Lost the Game";
+    } else {
+        return "You Tied the Game"
+    }
+}
+
+
+console.log(playGame())
+
